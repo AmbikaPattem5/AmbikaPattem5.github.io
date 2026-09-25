928b66fa4973488a2d2c06af8402eec7b400575c
Ambika Pattem <ambikapattem5@gmail.com>
2026-09-25 10:24:50 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // Set VITE_BASE_PATH=/your-repo-name/ when deploying to GitHub Project Pages
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH ?? '/',
+  base: '/portfolio/' ?? '/',
   plugins: [react(), tailwindcss()],
 })
